d5fc894bdf91a25e86b22ca48e6ddeca3b34ca37
nitishratre45 <nitishratre45@gmail.com>
2026-09-17 19:06:34 +0530

    save

--- cricketlive/app.js
+++ cricketlive/app.js
@@ -1,7 +1,7 @@
 // ===============================
 // PUBLIC HLS STREAM
 // ===============================
-const STREAM_URL = "https://jacksonville-seq-ingredients-accessible.trycloudflare.com/live/index.m3u8";
+const STREAM_URL = "https://administrator-information-emotional-sorts.trycloudflare.com/live/video1_stream.m3u8";
 
 
 // ===============================
