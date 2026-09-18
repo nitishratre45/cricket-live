403ca9f13053ff05f38a9a60d86d0a8e7bf95740
nitishratre45 <nitishratre45@gmail.com>
2026-09-18 09:09:27 +0530

    Update STREAM_URL to new live stream source

--- cricketlive/app.js
+++ cricketlive/app.js
@@ -1,8 +1,7 @@
 // ===============================
 // PUBLIC HLS STREAM
 // ===============================
-const STREAM_URL = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
-
+const STREAM_URL = "https://while-secretariat-alive-sims.trycloudflare.com/live/index.m3u8";
 // ===============================
 // VIDEO PLAYER
 // ===============================
